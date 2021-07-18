0fabcfac24e9c6467187bc0f9eea2bf57d439138
JansenJoris <joris_jansen@me.com>
2021-07-18 23:44:24 +0200

    sdsds

--- next.config.js
+++ next.config.js
@@ -26,3 +26,9 @@ module.exports = {
 module.exports = {
     target: 'serverless'
 }
+module.exports = {
+    env: {
+        MONGODB_URI: 'mongodb+srv://Joris:29911Linleoni4@cluster0.v9lde.mongodb.net/horecastaffDb?retryWrites=true&w=majority',
+        MONGODB_DB: 'horecastaffDb'
+    },
+}
